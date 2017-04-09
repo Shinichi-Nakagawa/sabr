811b5159300485588e70bb9f2c566e540d8f9473
shinyorke <spirits.is.my.rader@gmail.com>
2017-04-09 14:57:14 +0900

    add to setup.py,documents

---
+++ README.rst
@@ -0,0 +1,29 @@
+====================
+sabr
+====================
+
+SABRmetrics calculator for Python
+
+Requirement
+====================
+
+python 2.7+
+
+Install
+====================
+
+    $ pip install sabr
+
+Usage
+====================
+
+.. code-block:: python
+
+    from sabr.stats import Stats
+
+    h9 = Stats.hr9(26, 209.7)  # Yu Darvish(2013) HR/9
+
+License
+====================
+
+MIT License http://opensource.org/licenses/MIT
---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+__author__ = 'Shinichi Nakagawa'
+
+
+def read_file(filename):
+    basepath = os.path.dirname(os.path.dirname(__file__))
+    filepath = os.path.join(basepath, filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    return ''
+
+
+setup(
+    name='sabr',
+    version='1.0',
+    description='SABRmetrics calculator for Python',
+    long_description=read_file('README.rst'),
+    author='Shinichi Nakagawa',
+    author_email='spirits.is.my.rader@gmail.com',
+    url='https://github.com/Shinichi-Nakagawa/sabr',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    keywords=['baseball', 'MLB', 'SABRmetrics', 'SABR', 'Major league baseball'],
+    license='MIT License',
+    install_requires=[
+    ],
+)
